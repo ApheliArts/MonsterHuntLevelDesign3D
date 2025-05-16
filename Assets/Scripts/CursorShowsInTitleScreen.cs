e49956dedf3e3e3c26cb5f29dbb2af0176cece08
Natalie Daniel <wolfbrooke03@gmail.com>
2025-05-16 12:27:06 -0500

    Revert "Everything Works Now(?)"
    This reverts commit 082574ac60f049078a8b3774a17d06b6a22a5ada.

--- Assets/Scripts/CursorShowsInTitleScreen.cs
+++ Assets/Scripts/CursorShowsInTitleScreen.cs
@@ -5,8 +5,7 @@ public class CursorShowsInTitleScreen : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Cursor.lockState = CursorLockMode.None;
-        Time.timeScale = 1;
+        Cursor.visible = true;
     }
 
     // Update is called once per frame
