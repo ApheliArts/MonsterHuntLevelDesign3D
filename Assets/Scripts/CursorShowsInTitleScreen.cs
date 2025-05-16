082574ac60f049078a8b3774a17d06b6a22a5ada
Natalie Daniel <wolfbrooke03@gmail.com>
2025-05-16 12:23:19 -0500

    Everything Works Now(?)

--- Assets/Scripts/CursorShowsInTitleScreen.cs
+++ Assets/Scripts/CursorShowsInTitleScreen.cs
@@ -5,7 +5,8 @@ public class CursorShowsInTitleScreen : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+        Time.timeScale = 1;
     }
 
     // Update is called once per frame
